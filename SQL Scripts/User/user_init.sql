13437873b5e4421e284d4eed4774710e45e959c6
pjaraba <pe.jaraba@hotmail.com>
2016-07-19 13:14:03 -0500

    Atom json build targets were defined and database developing tools for user simple_delivery (logger and tapigen2) were included in tools_init.sql, user init script user_init.sql was created

---
+++ SQL Scripts/User/user_init.sql
@@ -0,0 +1,7 @@
+DROP USER simple_delivery CASCADE;
+CREATE USER simple_delivery IDENTIFIED BY 123456;
+GRANT DBA TO simple_delivery;
+GRANT connect,create view, create job, create table, create sequence, create trigger, create procedure, create any context TO simple_delivery;
+GRANT EXECUTE ON dbms_crypto TO simple_delivery;
+/
+@"SQL Scripts\User\tools_init.sql"

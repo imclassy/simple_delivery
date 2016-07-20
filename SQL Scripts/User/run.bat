9f0a8385b65f7e43a61644f4c5ea73ba20e08725
pjaraba <pe.jaraba@hotmail.com>
2016-07-20 15:58:25 -0500

    Configure installation with logger sources

--- SQL Scripts/User/run.bat
+++ SQL Scripts/User/run.bat
@@ -3,6 +3,9 @@ set vHost=localhost
 set vPortNo=1521
 set vServiceName=pdborcl as sysdba
 set vUserName=sys
-set vPassword=123456
+set vPassword=Pello1994
 echo @"%~dp0/user_init.sql" | sqlplus -S %vUserName%/%vPassword%@%vHost%:%VPortNo%/%vServiceName%
-echo @"%~dp0/tools_init.sql"| sqlplus -S %vUserName%/%vPassword%@%vHost%:%VPortNo%/%vServiceName% 
+set vServiceName=pdborcl
+set vUserName=simple_delivery
+set vPassword=123456
+echo @"%~dp0/logger_3.1.0/logger_install.sql"| sqlplus -S %vUserName%/%vPassword%@%vHost%:%VPortNo%/%vServiceName%

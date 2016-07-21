2e4e6ec35b6d216b5da161218c776c8264cb8b0f
pjaraba <pe.jaraba@hotmail.com>
2016-07-21 07:35:54 -0500

    added utplsql  for tools

--- SQL Scripts/User/user_init.sql
+++ SQL Scripts/User/user_init.sql
@@ -3,4 +3,10 @@ CREATE USER simple_delivery IDENTIFIED BY 123456;
 GRANT DBA TO simple_delivery;
 GRANT connect,create view, create job, create table, create sequence, create trigger, create procedure, create any context TO simple_delivery;
 GRANT EXECUTE ON dbms_crypto TO simple_delivery;
+
+DROP USER simple_delivery_utp CASCADE;
+CREATE USER simple_delivery_utp IDENTIFIED BY 123456;
+GRANT create session, create table, create procedure,
+  create sequence, create view, create public synonym,
+  drop public synonym to simple_delivery_utp;
 /

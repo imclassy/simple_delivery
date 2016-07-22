b21b147539e14ba73aaf100b0cb2ef16d9264c39
pjaraba <pe.jaraba@hotmail.com>
2016-07-22 15:23:08 -0500

    Changed log extnsion to .log in install.bat

--- install.bat
+++ install.bat
@@ -2,10 +2,10 @@
 echo Starting installation
 del install_log.txt
 echo Initializing database users
-call "SQL Scripts\User\run.bat" >> install_log.txt
+call "SQL Scripts\User\run.bat" >> install.log
 echo Initializing tables
-call "%~dp0SQL Scripts\Tables\run.bat" >> %~dp0install_log.txt
+call "%~dp0SQL Scripts\Tables\run.bat" >> %~dp0install.log
 echo Initializing packages
-call "%~dp0Packages\run.bat" >> %~dp0install_log.txt
+call "%~dp0Packages\run.bat" >> %~dp0install.log
 echo Set up completed! Check the install_log for installation results.
 pause

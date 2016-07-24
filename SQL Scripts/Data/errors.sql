674e7d3af9103c36af6803a3a3df579bb3d82661
pjaraba <pe.jaraba@hotmail.com>
2016-07-24 14:48:11 -0500

    Added Initial data scripts for installation

---
+++ SQL Scripts/Data/errors.sql
@@ -0,0 +1,4 @@
+INSERT INTO errors (error_id, message) VALUES (error_nums.invalid_user_type, 'Invalid user type');
+INSERT INTO errors (error_id, message) VALUES (error_nums.invalid_error_id, 'Invalid error id');
+COMMIT;
+/
